e1018679f8cd66db7a29e76dea71ec4a39a77394
Recep Taha Ayvaz <rtaha.ayva@gmail.com>
2026-04-23 20:11:36 +0300

    feat: created DatabaseHelper and User model files

---
+++ EvicomApp/EvicomApp/Data/DatabaseHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace EvicomApp.Data
+{
+    internal class DatabaseHelper
+    {
+        private static string dbPath = "evicom.db";
+        private static string connectionString = $"Data Source={dbPath};Version=3;";
+    }
+}
---
+++ EvicomApp/EvicomApp/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvicomApp.Models
+{
+    internal class User
+    {
+    }
+}
