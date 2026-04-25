56332917d48074105aa7b7f6f094ff7361488449
Recep Taha Ayvaz <rtaha.ayva@gmail.com>
2026-04-25 18:05:22 +0300

    feat: implement properties and relationships in Comment model

---
+++ EvicomApp/EvicomApp/Models/Comment.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace EvicomApp.Models
+{
+    internal class Comment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int SourceId { get; set; }
+        public string SourceType { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User User { get; set; }
+    }
+}
