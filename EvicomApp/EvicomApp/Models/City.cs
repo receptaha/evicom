91d434ed352499a880d7f2e1edf8a7c12f4a27c7
Recep Taha Ayvaz <rtaha.ayvaz@gmail.com>
2026-04-26 04:23:46 +0300

    feat: implement properties and relationships in City and District Model

---
+++ EvicomApp/EvicomApp/Models/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvicomApp.Models
+{
+    internal class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<District> Districts {get; set;} = new List<District>();
+    }
+}
